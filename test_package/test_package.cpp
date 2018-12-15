b79576251767d3d0cf640ab8493649a0b712a30c
Jerry W <jerrywiltse@gmail.com>
2018-12-15 00:57:26 -0500

    add real test package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,17 @@
-#include <cstdlib>
+#include <osg/Polytope>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Bincrafters\n";
-    return EXIT_SUCCESS;
+    osg::Polytope pt;
+    pt.setToBoundingBox(osg::BoundingBox(-1000, -1000, -1000, 1000, 1000, 1000));
+    bool bContains = pt.contains(osg::Vec3(0, 0, 0));
+    if (bContains)
+    {
+        std::cout<<"Polytope pt.contains(osg::Vec3(0, 0, 0)) has succeeded."<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Polytope pt.contains(osg::Vec3(0, 0, 0)) has failed."<<std::endl;
+    }
 }
