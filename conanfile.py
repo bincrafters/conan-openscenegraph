60d58aa5525810f7fe8d4af78f11ce7665912935
Jerry Wiltse <jwiltse@carsim.com>
2018-12-17 10:11:46 -0500

    try turnoff example building, and fix MT

--- conanfile.py
+++ conanfile.py
@@ -29,7 +29,8 @@ class OpenscenegraphConan(ConanFile):
         "shared": False, 
         "fPIC": True,
         "build_osg_applications": False, 
-        "build_osg_plugins": False, 
+        "build_osg_plugins_by_default": False, 
+        "build_osgexamples": False, 
     }
     _sha256_checksum = "51bbc79aa73ca602cd1518e4e25bd71d41a10abd296e18093a8acfebd3c62696"
     _source_subfolder = "source_subfolder"
@@ -66,10 +67,14 @@ class OpenscenegraphConan(ConanFile):
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.verbose = True
-        cmake.definitions["BUILD_OSG_APPLICATIONS"] = "ON"  if self.options.build_osg_applications else "OFF"
-        cmake.definitions["DYNAMIC_OPENSCENEGRAPH"] = "ON"  if self.options.shared else "OFF"
-        cmake.definitions["BUILD_OSG_PLUGINS_BY_DEFAULT"] = "ON"  if self.options.build_osg_plugins else "OFF"
+        cmake.definitions["BUILD_OSG_APPLICATIONS"] = "ON" if self.options.build_osg_applications else "OFF"
+        cmake.definitions["DYNAMIC_OPENSCENEGRAPH"] = "ON" if self.options.shared else "OFF"
+        cmake.definitions["BUILD_OSG_PLUGINS_BY_DEFAULT"] = "ON" if self.options.build_osg_plugins_by_default else "OFF"
+        cmake.definitions['BUILD_OSGEXAMPLES'] = "ON" if self.options.build_osgexamples else "OFF"
+        
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions['BUILD_WITH_STATIC_CRT']= "ON" if "MT" in str(self.settings.compiler.runtime) else "OFF"
+        
         cmake.configure()
         return cmake
 
